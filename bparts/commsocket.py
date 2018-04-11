82727b6ed60843db67458aac49155412243c2404
berthoud <berthoud@dhcp-10-105-9-60.wireless.northwestern.private>
2018-04-11 15:42:16 -0500

    Commsocket first version

---
+++ bparts/commsocket.py
@@ -0,0 +1,18 @@
+"""
+CommSocket
+==========
+
+This library contains functions for one- and two-way communication
+between different programs over sockets. 
+
+"""
+
+def socksend(message, port, server = '127.0.0.1'):
+    """ Sends 
+    """
+    pass
+
+def sockrecv(message, port, server = '127.0.0.1'):
+    """ Sends and receives
+    """
+    
