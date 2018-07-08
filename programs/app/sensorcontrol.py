980d0dabc95c2e3efa115a64136c0653c66083ac
adhuber <adamrhuber@gmail.com>
2018-07-08 15:35:04 -0500

    Updated sensor pagelet to match comm doc

--- programs/app/sensorcontrol.py
+++ programs/app/sensorcontrol.py
@@ -4,5 +4,5 @@ from wtforms import SelectField
 class sensorForm(FlaskForm):
 	sensor = SelectField(
 		'Sensor',
-		choices=[('time','Time'),('test','Test'),('dist1','Distance Sensor 1'),('dist2','Distance Sensor 2'),('dist3','Distance Sensor 3'),('dist4','Distance Sensor 4'),('head','Heading'),('pitch','Pitch'),('roll','Roll')]
+		choices=[('time','Time'),('test','Test'),('analog1','Analog 1'),('analog2','Analog 2'),('analog3','Analog 3'),('analog4','Analog 4'),('head','Heading'),('pitch','Pitch'),('roll','Roll')]
 	)
