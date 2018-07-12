35cdf1efc170235154da25b6dfc7b30a5f186acb
berthoud <berthoud@localhost>
2018-07-12 12:16:46 -0500

    Minor commit

--- startflying.sh
+++ startflying.sh
@@ -16,21 +16,21 @@ screen -S flyscreen -X screen -t flask
 ## Start the programs
 
 # Run output
-screen -S flyscreen -p output -X stuff 'cd techcamp/programs
+screen -S flyscreen -p output -X stuff 'cd ~/techcamp/programs
 python3 output.py ../configs/master.ini
 '
 # Display log
-screen -S flyscreen -p log -X stuff 'tail -f space_log.txt
+screen -S flyscreen -p log -X stuff 'tail -f ~/space_log.txt
 '
 # Run incam
-screen -S flyscreen -p incam -X stuff 'cd techcamp/programs
+screen -S flyscreen -p incam -X stuff 'cd ~/techcamp/programs
 python3 incam.py ../configs/master.ini
 '
 # Run insensor
-screen -S flyscreen -p insense -X stuff 'cd techcamp/programs
+screen -S flyscreen -p insense -X stuff 'cd ~/techcamp/programs
 python3 insensor.py ../configs/master.ini
 '
 # Run flask
-screen -S flyscreen -p flask -X stuff 'cd techcamp/programs
+screen -S flyscreen -p flask -X stuff 'cd ~/techcamp/programs
 python flaskserver.py
 '
