7b926ad30c2e4146b4c6e2b4e0066394ab62f705
adhuber <adamrhuber@gmail.com>
2018-07-13 10:15:41 -0500

    Added heading page

--- programs/app/routes.py
+++ programs/app/routes.py
@@ -59,6 +59,11 @@ def droneterm():
 	if request.method == 'POST':
 		send_command(form.command.data)
 	return render_template('droneterm.html',title='Send Command',form=form)
+@app.route('/heading',methods=['GET','POST'])
+def heading:
+	if request.method=='POST':
+		send_command('insense heading')
+	return render_template('heading.html')
 @app.route('/stream')
 def stream():
 	return render_template('stream.html')
